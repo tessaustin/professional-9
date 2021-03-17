76bee29d2514e66a910d94b8909a789b24fc5da5
Tess Austin <tessaustin92@gmail.com>
2021-03-17 12:35:53 -0600

    All optional questions layed out, still need licensing

--- index.js
+++ index.js
@@ -7,18 +7,77 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 const questions = [
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name? (Required)',
-        //make sure user input something
-        validate: nameInput => {
-            if (nameInput) {
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        //make sure user entered input
+        validate: githubInput => {
+            if (githubInput) {
                 return true;
             } else {
-                console.log('Please enter your name!');
+                console.log('Must enter your GitHub username!');
                 return false;
             }
         }
     },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your profile. (Required)',
+        //make sure user entered input
+        validate: linkInput => {
+            if (linkInput) {
+                return true;
+            } else {
+                console.log('You must enter a your profile GitHub link!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address. (Required)',
+        //make sure user entered input
+        validate: linkInput => {
+            if (linkInput) {
+                return true;
+            } else {
+                console.log('You must enter a your email address!');
+                return false;
+            }
+        }
+    },
+    //Project title
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
+        //make sure user entered input
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('You must enter a project name!');
+                return false;
+            }
+        }
+    },
+    //Project Desciption
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        //make sure user entered input
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You must enter a project description!');
+                return false;
+            }
+        }
+    },
+
 ];
 
 // TODO: Create a function to write README file

ff0fb36ed9e135c097f2f5a828424e40090897ff
Tess Austin <tessaustin92@gmail.com>
2021-03-17 16:39:44 -0600

    trying to get markdown to bold word...

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
 
-    ## Description:
+## Description:
     ${data.description}
 
     ## Licensing:
@@ -45,7 +45,8 @@ function generateMarkdown(data) {
 
     ## Questions:
     - Github: [${data.github}](https://github.com/${data.github})
-    - Email: ${data.email} `;
-  }
-  
-  module.exports = generateMarkdown;
+    - Email: ${data.email} 
+    `;
+}
+
+module.exports = generateMarkdown;

f9760fd9ee341e40904bd0824150bab2b3d9c6cf
Tess Austin <tessaustin92@gmail.com>
2021-03-17 12:03:16 -0600

    layed out questions to answer that will be put under the questions section

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        //make sure user input something
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
